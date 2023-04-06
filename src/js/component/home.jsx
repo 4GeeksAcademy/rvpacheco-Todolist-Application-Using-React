7846329bf4ee2a40d7ea33546adb21cca49b4197
rvpacheco <rvpacheco17@gmail.com>
2023-04-06 02:49:19 +0000

    commit terminado

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,52 @@
-import React from "react";
+import React, {useState} from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [task, setTask]=useState("")
+	const [todos, setTodos]=useState([])
+	function addTask (e){
+		if(e.code =="Enter"){
+			setTodos([...todos, task]);
+			setTask("");
+		}
+
+	}
+
+	function delTask(index){
+		setTodos(prevTodos => {
+			const newTodos = [...prevTodos];
+			newTodos.splice(index, 1);
+			return newTodos;
+		  });
+	}
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+				<div className="card" style={{width: "18rem"}}>
+					<div className="card-header">
+						<input 
+							type="text" 
+							className="form-control border-0" 
+							id="exampleFormControlInput1" 
+							placeholder="Escriba una nueva tarea" 
+							value={task} 
+							onChange={(e)=>setTask(e.target.value)} 
+							onKeyDown={addTask}
+							/>
+					</div>
+				<ul className="list-group list-group-flush">
+					{todos.map((todo,index) => 
+						<li className="list-group-item d-flex justify-content-between align-item">
+							{todo}
+							<button className="btn btn-outline-danger btn-sm rounded-pill" onClick={()=>delTask(index)}	>X</button>
+						</li> 
+					)}
+				</ul>
+				<div className="card-footer">Tareas {todos.length}</div>
+				</div>
+
 	);
 };
 
