94e9f8053326b474d68add1a2045567b66b4ae9d
rvpacheco <124650306+rvpacheco@users.noreply.github.com>
2023-04-25 01:01:06 +0000

    terminado

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,27 +1,106 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
+
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	
+	
 	const [task, setTask]=useState("")
 	const [todos, setTodos]=useState([])
-	function addTask (e){
-		if(e.code =="Enter"){
-			setTodos([...todos, task]);
-			setTask("");
+	const apiUrl="https://assets.breatheco.de/apis/fake/todos/user/rvpacheco"
+	async function loadList(){
+		let response=await fetch(apiUrl)
+		if (response.ok){
+			let data=await response.json()
+			setTodos(data)
 		}
+		return response.status
 
 	}
+	
+	useEffect(()=>{
+		
+		loadList().then(async status=>{
+			if(status==404){
+				let response = await fetch(apiUrl, {
+					method: "POST",
+					body: "[]",
+					headers: {
+						"Content-Type": "application/json"
+					}
+    			})	
+				if (response.ok) return loadList()
+			}
+		})
+	},[])
+	
+	function addTask(e) {
+  if (e.code === "Enter") {
+    let newTask = { label: e.target.value, done: false };
+    let newTodos = [...todos, newTask];
+    fetch(apiUrl, {
+      method: "PUT",
+      body: JSON.stringify(newTodos),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return response.json();
+    })
+    .then(data => {
+      setTodos(newTodos);
+      e.target.value = "";
+    })
+    .catch(error => {
+      console.error("There was a problem with the fetch operation:", error);
+    });
+  }
+}
 
-	function delTask(index){
-		setTodos(prevTodos => {
-			const newTodos = [...prevTodos];
-			newTodos.splice(index, 1);
-			return newTodos;
-		  });
+	  
+	  
+
+	
+function delTask(index) {
+	let newTodos = [...todos];
+	newTodos.splice(index, 1);
+	fetch(apiUrl, {
+	  method: "PUT",
+	  body: JSON.stringify(newTodos),
+	  headers: {
+		"Content-Type": "application/json"
+	  }
+	})
+	.then(response => {
+	  if (!response.ok) {
+		throw new Error("Network response was not ok");
+	  }
+	  return response.json();
+	})
+	.then(data => {
+	  setTodos(newTodos);
+	})
+	.catch(error => {
+	  console.error("There was a problem with the fetch operation:", error);
+	});
+  }
+
+	function checkTodo(index){
+		let newTodos=[...todos]
+		newTodos[index].done=!newTodos[index].done
+		setTodos(newTodos)
 	}
+
+
+	
+
 
 	return (
 				<div className="card" style={{width: "18rem"}}>
@@ -36,14 +115,24 @@ const Home = () => {
 							onKeyDown={addTask}
 							/>
 					</div>
-				<ul className="list-group list-group-flush">
+				<ol className="list-group list-group-flush">
 					{todos.map((todo,index) => 
-						<li className="list-group-item d-flex justify-content-between align-item">
-							{todo}
+						
+						<li key = {index} className="list-group-item d-flex justify-content-between align-item">
+							<div>
+								<input 
+								className="form-check-input me-3"
+								type="checkbox"
+								onChange={()=>checkTodo(index)}
+								checked={todo.done}
+								/>
+								{todo.label}
+							</div>
+							
 							<button className="btn btn-outline-danger btn-sm rounded-pill" onClick={()=>delTask(index)}	>X</button>
 						</li> 
 					)}
-				</ul>
+				</ol>
 				<div className="card-footer">Tareas {todos.length}</div>
 				</div>
 
